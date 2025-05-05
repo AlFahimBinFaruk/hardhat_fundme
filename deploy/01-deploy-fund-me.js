5871db2422081b4ce8ffcb1738218680f847fd1e
AlFahimBinFaruk <alfahim.ps@gmail.com>
2025-05-05 12:48:03 +0600

    feat:contract verfiation added

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,5 +1,6 @@
 const {network}=require("hardhat");
 const {developmentChains,networkConfig}=require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
 
 
 module.exports=async({getNamedAccounts,deployments})=>{
@@ -16,18 +17,26 @@ module.exports=async({getNamedAccounts,deployments})=>{
         ethUSDPriceFeedAddress=networkConfig[chainId]["ethUsdPriceFeed"];
     }
 
-    // log(`address is ===> `,ethUSDPriceFeedAddress);
+    
     log("----------------------------------------------------")
     log("Deploying FundMe and waiting for confirmations...")
+
+    let arguments=[ethUSDPriceFeedAddress];
     const fundMe=await deploy("FundMe",{
         contract:"FundMe",
         from:deployer,
         log:true,
-        args:[ethUSDPriceFeedAddress],
+        args:arguments,
         waitConfirmations:network.config.blockConfirmations||1
     })
 
     log(`Fund me deployed at ==> `,fundMe.address);
+
+
+    if(!developmentChains.includes(network.name)){
+        await verify(fundMe.address,arguments);
+    }
+
 }
 
 module.exports.tags=["all","fundme"];
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,42 @@
 require("hardhat-deploy");
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-ethers");
+require("@nomicfoundation/hardhat-verify");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+
+
+const SEPOLIA_RPC_URL=process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY=process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY=process.env.ETHERSCAN_API_KEY;
+
+
+
 module.exports = {
+
   solidity: "0.8.28",
+
+  defaultNetwork:"hardhat",
+
+  networks:{
+    sepolia:{
+      url:SEPOLIA_RPC_URL,
+      accounts:[PRIVATE_KEY],
+      chainId:11155111
+    }
+  },
+
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  },
+
   namedAccounts:{
     deployer:{
       default:0,
       1:0
     }
   },
+
+
+
 };
